d89988dbb079df6a6c25da52d9a96536bcc7c5e2
Michella Christensen <michellachristensen@MacBook-Air-tilhrende-Michella.local>
2023-08-25 00:04:16 +0200

    Add more swiper stuffs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,13 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     experimental: {
-        viewTransitions: true
-    }
+        viewTransitions: true,
+        assets: true,
+    },
+    vite: {
+        ssr: {
+          external: ['swiper'],
+        }
+      }
+    
 });
