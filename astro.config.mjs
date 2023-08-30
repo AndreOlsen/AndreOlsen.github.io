f69e936864564cce62cb9bd532493fd51cb6a7c1
André Olsen <andre_w.o@outlook.com>
2023-08-30 22:25:47 +0200

    Add missing config option

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://andreolsen.github.io',
   experimental: {
       viewTransitions: true,
       assets: true,
