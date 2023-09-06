cce0d52ddae7f2b4386fc23568dd82bb06c10d79
André Olsen <andre_w.o@outlook.com>
2023-09-06 21:46:03 +0200

    More sutffs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,7 +16,7 @@ module.exports = {
 				fadeInUp: {
 					'0%': {
 						transform: 'translateY(100%)',
-						opacity: 0.01
+						opacity: 0,
 					},
 					'100%': {
 						transform: 'translateY(0)',
