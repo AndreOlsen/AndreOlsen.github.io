9795e5b172b3164605de3b25952b4282dc2dc0f5
André Olsen <andre_w.o@outlook.com>
2023-11-14 18:35:43 +0100

    Updated Astro to 3.5

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,4 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: 'https://andreolsen.github.io',
   integrations: [sitemap(), tailwind()],
-  vite: {
-    ssr: {
-      noExternal: ['rellax'],
-    }
-  }
 });
